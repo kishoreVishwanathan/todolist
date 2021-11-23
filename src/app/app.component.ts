e505bdcb15d209d6aa33f85661aac5346a720631
kishore <kishorepranesh676@gmail.com>
2021-11-23 10:00:16 +0000

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularapp';
+  todoArray: Array<any> = [];
+  todoItem: string = '';
+
+
+  addTodoList() {
+    if(this.todoItem) {
+      this.todoArray.push({
+        id: this.todoArray.length,
+        item: this.todoItem
+      });
+    this.todoItem = '';
+     } 
+  }
+
+  removeTodo(itemId: number) {
+    this.todoArray =  this.todoArray.filter((item: any) => item.id !== itemId);
+  }
 }
